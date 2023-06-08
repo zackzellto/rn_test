ca65fbefcaa96bd8b6eeeaf423dfc7e728e95d97
Zack Zellto <zacharyzellto@gmail.com>
2023-06-07 22:11:02 -0600

    installed nativewind

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to fmly!</Text>
+    <View class="flex-1 items-center justify-center bg-white">
+      <Text>Welcome to fmly !!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#4FEEC8",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
